b88ffed2b3e531bd3cd3e831b1d772ff3c9a247d
Gustavo Silva <gustavo.silva@aptoide.com>
2018-07-18 11:37:35 +0100

    [new] Dockerfile for python

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #   build ui container
-docker run -it -v ${PWD}/ui:/usr/src/ui -v /usr/src/ui/node_modules -p 3000:3000 --rm   lnhw_ui
+docker run -d -v ${PWD}/rpc:/usr/src/rpc -p 5000:5000 --rm lnhw_rpc && \
+docker run -it -v ${PWD}/ui:/usr/src/ui -v /usr/src/ui/node_modules -p 3000:3000 --rm lnhw_ui
